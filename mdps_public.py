ab63b205c00eabe49f3f70d8a2f1b2047c58775f
yash123-vis <117199263+yash123-vis@users.noreply.github.com>
2023-05-29 20:25:46 +0530

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -12,11 +12,11 @@ from streamlit_option_menu import option_menu
 
 # loading the saved models
 
-diabetes_model = pickle.load(open('C:/Users/DELL/OneDrive/Desktop/multiple disease prediction/diabetes_model.sav', 'rb'))
+diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
 
-heart_disease_model = pickle.load(open('C:/Users/DELL/OneDrive/Desktop/multiple disease prediction/heart_disease_model.sav','rb'))
+heart_disease_model = pickle.load(open('heart_disease_model.sav','rb'))
 
-parkinsons_model = pickle.load(open('C:/Users/DELL/OneDrive/Desktop/multiple disease prediction/parkinsons_model.sav', 'rb'))
+parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
 
 
 
